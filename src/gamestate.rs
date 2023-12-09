3df25baca571518529f7cd54dfae15804f91fe18
Kim Pedersen <Kim Pedersen>
2023-12-09 12:42:02 -0500

    Moved resources to static byte arrays

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -1,4 +1,4 @@
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::{self, Color, Texture, ImageData};
 use tetra::graphics::text::{Font, Text};
 use tetra::input::{self, Key};
 use tetra::math::Vec2;
@@ -26,25 +26,28 @@ pub struct GameState {
 
 impl GameState {
     pub fn new(ctx: &mut Context) -> tetra::Result<GameState> {
-        let player1_texture = Texture::new(ctx, "./resources/player1.png")?;
+        let player1_imagedata = ImageData::from_encoded(include_bytes!("../resources/player1.png"))?;
+        let player1_texture = Texture::from_image_data(ctx, &player1_imagedata)?;
         let player1_position = Vec2::new(
             16.0,
             (WINDOW_HEIGHT - player1_texture.height() as f32) / 2.0,
         );
-
-        let player2_texture = Texture::new(ctx, "./resources/player2.png")?;
+        let player2_imagedata = ImageData::from_encoded(include_bytes!("../resources/player2.png"))?;
+        let player2_texture = Texture::from_image_data(ctx, &player2_imagedata)?;
         let player2_position = Vec2::new(
             WINDOW_WIDTH - player2_texture.width() as f32 - 16.0,
             (WINDOW_HEIGHT - player2_texture.height() as f32) / 2.0,
         );
-
-        let ball_texture = Texture::new(ctx, "./resources/ball.png")?;
+        let ball_imagedata = ImageData::from_encoded(include_bytes!("../resources/ball.png"))?;
+        let ball_texture = Texture::from_image_data(ctx, &ball_imagedata)?;
         let ball_position = Vec2::new(
             WINDOW_WIDTH / 2.0 - ball_texture.width() as f32 / 2.0,
             WINDOW_HEIGHT / 2.0 - ball_texture.height() as f32 / 2.0,
         );
         let ball_velocity = Vec2::new(BALL_SPEED as f32, 0 as f32);
-        let font = Font::vector(ctx, "./resources/DejaVuSansMono.ttf", 24.0)?;
+
+        let font_bytes = include_bytes!("../resources/DejaVuSansMono.ttf");
+        let font = Font::from_vector_file_data(ctx, font_bytes, 24.0)?;
 
         Ok(GameState {
             player1: Player::new(player1_texture, player1_position),
@@ -58,90 +61,41 @@ impl GameState {
 
     fn reset_game(&mut self) {
         self.game_over = false;
-        self.ball.puck.position = Vec2::new(
-            WINDOW_WIDTH / 2.0 - self.ball.puck.width() / 2.0,
-            WINDOW_HEIGHT / 2.0 - self.ball.puck.height() / 2.0,
-        );
-        self.player1.paddle.position = Vec2::new(
-            16.0,
-            (WINDOW_HEIGHT - self.player1.paddle.height()) / 2.0,
-        );
-        self.player2.paddle.position = Vec2::new(
-            WINDOW_WIDTH - self.player2.paddle.width() - 16.0,
-            (WINDOW_HEIGHT - self.player2.paddle.height()) / 2.0,
-        );
-        self.ball.velocity = Vec2::new(BALL_SPEED, 0.0);
+        self.ball.reset();
+        self.player1.reset();
+        self.player2.reset();
     }
 
     fn update_positions(&mut self, ctx: &mut Context) {
         if input::is_key_down(ctx, Key::W) {
-            self.player1.paddle.position.y -= PADDLE_SPEED;
+            self.player1.move_up()
         }
         if input::is_key_down(ctx, Key::S) {
-            self.player1.paddle.position.y += PADDLE_SPEED;
+            self.player1.move_down();
         }
         if input::is_key_down(ctx, Key::Up) {
-            self.player2.paddle.position.y -= PADDLE_SPEED;
+            self.player2.move_up();
         }
         if input::is_key_down(ctx, Key::Down) {
-            self.player2.paddle.position.y += PADDLE_SPEED;
+            self.player2.move_down();
         }
-        if self.player1.paddle.position.y < 0.0 {
-            self.player1.paddle.position.y = 0.0;
-        }
-        if self.player1.paddle.position.y + self.player1.paddle.height() > WINDOW_HEIGHT {
-            self.player1.paddle.position.y = WINDOW_HEIGHT - self.player1.paddle.height();
-        }
-        if self.player2.paddle.position.y < 0.0 {
-            self.player2.paddle.position.y = 0.0;
-        }
-        if self.player2.paddle.position.y + self.player2.paddle.height() > WINDOW_HEIGHT {
-            self.player2.paddle.position.y = WINDOW_HEIGHT - self.player2.paddle.height();
-        }
-        self.ball.puck.position += self.ball.velocity;
-        if self.ball.puck.position.y <= 0.0 || self.ball.puck.position.y + self.ball.puck.height() >= WINDOW_HEIGHT {
-            self.ball.velocity.y = -self.ball.velocity.y;
-        }
+        self.ball.move_puck();
     }
 
     fn check_paddle_hit(&mut self) {
-        let player1_bounds = self.player1.paddle.bounds();
-        let player2_bounds = self.player2.paddle.bounds();
-        let ball_bounds = self.ball.puck.bounds();
-
-        let paddle_hit = if ball_bounds.intersects(&player1_bounds) {
-            Some(&self.player1.paddle)
-        } else if ball_bounds.intersects(&player2_bounds) {
-            Some(&self.player2.paddle)
-        } else {
-            None
-        };
-
-        if let Some(paddle) = paddle_hit {
-            // Increase the ball's velocity, then flip it.
-            self.ball.velocity.x =
-                -(self.ball.velocity.x + (BALL_ACC * self.ball.velocity.x.signum()));
-
-            // Calculate the offset between the paddle and the ball, as a number between
-            // -1.0 and 1.0.
-            let offset = (paddle.centre().y - self.ball.puck.centre().y) / paddle.height();
-
-            // Apply the spin to the ball.
-            self.ball.velocity.y += PADDLE_SPIN * -offset;
-        }
+        self.ball.hit(&self.player1);
+        self.ball.hit(&self.player2);
     }
 
     fn check_game_over(&mut self) {
-        if self.ball.puck.position.x < 0.0 {
+        if self.ball.is_in_p1_goal() {
             self.game_over = true;
-            self.player2.score += 1;
-            self.p2score_text.set_content(format!("Red: {}", self.player2.score));
-        }
-
-        if self.ball.puck.position.x > WINDOW_WIDTH {
+            self.player2.score();
+            self.p2score_text.set_content(format!("Red: {}", self.player2.get_score()));
+        } else if self.ball.is_in_p2_goal(){
             self.game_over = true;
-            self.player1.score += 1;
-            self.p1score_text.set_content(format!("Blue: {}", self.player1.score));
+            self.player1.score();
+            self.p1score_text.set_content(format!("Blue: {}", self.player1.get_score()));
         }
     }
 }
@@ -152,9 +106,9 @@ impl State for GameState {
         self.p1score_text.draw(ctx, Vec2::new(10.0, 10.0));
         self.p2score_text.draw(ctx, Vec2::new(WINDOW_WIDTH - 100 as f32, 10.0));
         if !self.game_over {
-            self.player1.paddle.texture.draw(ctx, self.player1.paddle.position);
-            self.player2.paddle.texture.draw(ctx, self.player2.paddle.position);
-            self.ball.puck.texture.draw(ctx, self.ball.puck.position);
+            self.player1.draw(ctx);
+            self.player2.draw(ctx);
+            self.ball.draw(ctx);
         }
         Ok(())
     }
--- src/gamestate/entity.rs
+++ src/gamestate/entity.rs
@@ -1,9 +1,14 @@
 use tetra::graphics::{Texture, Rectangle};
 use tetra::math::Vec2;
+use tetra::Context;
+
+use crate::gamestate::{WINDOW_HEIGHT, WINDOW_WIDTH, PADDLE_SPEED, PADDLE_SPIN, BALL_ACC};
+
+use super::BALL_SPEED;
 
 pub struct Entity {
-    pub texture: Texture,
-    pub position: Vec2<f32>,
+    texture: Texture,
+    position: Vec2<f32>,
 }
 
 impl Entity {
@@ -33,26 +38,102 @@ impl Entity {
             self.position.y + (self.height() / 2.0),
         )
     }
+
+    pub fn draw(&mut self, ctx: &mut Context) {
+        self.texture.draw(ctx, self.position);
+    }
 }
 
 pub struct Player {
-    pub paddle: Entity,
-    pub score: i32,
+    paddle: Entity,
+    score: i32,
 }
 
 pub struct Ball {
-    pub puck: Entity,
-    pub velocity: Vec2<f32>,
+    puck: Entity,
+    velocity: Vec2<f32>,
 }
 
 impl Player {
     pub fn new(texture: Texture, position: Vec2<f32>) -> Player {
         Player { paddle: Entity::new(texture, position), score: 0 as i32}
     }
+
+    pub fn move_up(&mut self) {
+        self.paddle.position.y -= PADDLE_SPEED;
+        if self.paddle.position.y < 0.0 {
+            self.paddle.position.y = 0.0;
+        }
+    }
+
+    pub fn move_down(&mut self) {
+        self.paddle.position.y += PADDLE_SPEED;
+        if self.paddle.position.y + self.paddle.height() > WINDOW_HEIGHT {
+            self.paddle.position.y = WINDOW_HEIGHT - self.paddle.height();
+        }
+    }
+
+    pub fn score(&mut self) {
+        self.score += 1;
+    }
+
+    pub fn draw(&mut self, ctx: &mut Context) {
+        self.paddle.draw(ctx);
+    }
+
+    pub fn get_score(&self) -> i32 {
+        self.score
+    }
+
+    pub fn reset(&mut self) {
+        self.paddle.position = Vec2::new(
+            self.paddle.position.x,
+            WINDOW_HEIGHT / 2.0 - self.paddle.height() / 2.0,
+        );
+    }
 }
 
 impl Ball {
     pub fn new(texture: Texture, position: Vec2<f32>, velovity: Vec2<f32>) -> Ball {
         Ball { puck: Entity::new(texture, position), velocity: velovity}
+    }
+
+    pub fn draw(&mut self, ctx: &mut Context) {
+        self.puck.draw(ctx);
+    }
+
+    pub fn move_puck(&mut self) {
+        self.puck.position += self.velocity;
+        if self.puck.position.y <= 0.0 || self.puck.position.y + self.puck.height() >= WINDOW_HEIGHT {
+            self.velocity.y = -self.velocity.y;
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.puck.position = Vec2::new(
+            WINDOW_WIDTH / 2.0 - self.puck.width() / 2.0,
+            WINDOW_HEIGHT / 2.0 - self.puck.height() / 2.0,
+        );
+        self.velocity = Vec2::new(BALL_SPEED, 0.0);
+    }
+
+    pub fn is_in_p1_goal(&self) -> bool {
+        self.puck.position.x < 0.0
+    }
+
+    pub fn is_in_p2_goal(&self) -> bool {
+        self.puck.position.x + self.puck.width() > WINDOW_WIDTH
+    }
+
+    pub fn hit(&mut self, player: &Player) {
+        if self.puck.bounds().intersects(&player.paddle.bounds()) {
+            // Increase the ball's velocity, then flip it.
+            self.velocity.x = -(self.velocity.x + (BALL_ACC * self.velocity.x.signum()));
+            // Calculate the offset between the paddle and the ball,
+            // as a number between -1.0 and 1.0.
+            let offset = (player.paddle.centre().y - self.puck.centre().y) / player.paddle.height();
+            // Apply the spin to the ball.
+            self.velocity.y += PADDLE_SPIN * -offset;
+        }
     }
 }
